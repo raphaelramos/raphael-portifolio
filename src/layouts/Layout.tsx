854fd5222c9b1e7fd0ce9d241958a6db78bd41bf
Raphael Ramos <raphael.ramos.br@gmail.com>
2023-09-25 19:04:22 -0300

    refactor: change location import dark.css

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,28 +1,35 @@
-import React, { ReactNode } from 'react'
-import Head from 'next/head'
-import { NextSeo } from 'next-seo'
+import React, { ReactNode } from "react";
+import { NextSeo } from "next-seo";
 
 type Props = {
-    children: ReactNode
-    title: string
-    description: string
-    imageUrl?: string
-}
+  children: ReactNode;
+  title: string;
+  description: string;
+  imageUrl?: string;
+};
 
-const Layout = ({ children, title, description, imageUrl }: Props): JSX.Element => (
-    <div>
-        <Head>
-            <link rel="stylesheet" href="/css/dark.css" />
-        </Head>
-        <NextSeo
-            title={title}
-            description={description}
-            openGraph={{ title, description, images: [{
-                url: imageUrl || '/img/thumb-site.png'
-            }]
-        }} />
-        {children}
-    </div>
-)
+const Layout = ({
+  children,
+  title,
+  description,
+  imageUrl,
+}: Props): JSX.Element => (
+  <div>
+    <NextSeo
+      title={title}
+      description={description}
+      openGraph={{
+        title,
+        description,
+        images: [
+          {
+            url: imageUrl || "/img/thumb-site.png",
+          },
+        ],
+      }}
+    />
+    {children}
+  </div>
+);
 
-export default Layout
+export default Layout;
