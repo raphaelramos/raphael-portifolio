e2d94b42fc81140c078bb835d3fcca907f130603
Raphael Ramos <raphael.ramos.br@gmail.com>
2024-04-15 15:21:43 -0300

    build: update depedences

--- next.config.js
+++ next.config.js
@@ -1,27 +1,23 @@
 const path = require('path')
 
 module.exports = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'css')],
-    },
-    trailingSlash: true,
-    devIndicators: {
-        buildActivity: false,
-    },
-    eslint: {
-        ignoreDuringBuilds: false,
-    },
-    images: {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "css")],
+  },
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'dev-to-uploads.s3.amazonaws.com'
+        protocol: "https",
+        hostname: "media.dev.to",
       },
     ],
   },
-}
+};
