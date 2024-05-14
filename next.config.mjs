688652325bc456b987766436e6c9b44be4a78ad0
r2company <raphael.ramos.br@gmail.com>
2024-05-14 00:58:05 -0300

    refactor: change project config

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
