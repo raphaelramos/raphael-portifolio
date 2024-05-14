e609bb21fffb1085574f2627d80e19b4f997419c
r2company <raphael.ramos.br@gmail.com>
2024-05-14 02:05:19 -0300

    feat: add gtm id .env variable

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import Script from "next/script";
-const GTM_ID = "GTM-5MLS8J3";
+
+const GOOGLE_TAG_MANAGER_ID = process.env.DEVTO_APIKEY;
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -44,15 +45,17 @@ class MyDocument extends Document {
             strategy="beforeInteractive"
             src="/js/isotope.pkgd.min.js"
           ></Script>
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `}
-          </Script>
+          {GOOGLE_TAG_MANAGER_ID && (
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
+              `}
+            </Script>
+          )}
         </body>
       </Html>
     );
