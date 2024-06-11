3e2cd7b97ad03ca2f39895b889d8f4c777202ff9
Raphael Ramos <raphael.ramos.br@gmail.com>
2024-06-11 01:00:08 -0300

    fix: google tag manage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import Cursor from "../components/cursor";
 import ScrollToTop from "../components/scrollToTop";
 import LoadingScreen from "../components/Loading-Screen";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -12,9 +13,14 @@ const poppins = Poppins({
   display: "swap",
 });
 
+const GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {GOOGLE_TAG_MANAGER_ID && (
+        <GoogleTagManager gtmId={GOOGLE_TAG_MANAGER_ID} />
+      )}
       <Cursor />
       <LoadingScreen />
       <main className={poppins.className}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,6 @@ import Document, {
 } from "next/document";
 import Script from "next/script";
 
-const GOOGLE_TAG_MANAGER_ID = process.env.DEVTO_APIKEY;
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -45,17 +43,6 @@ class MyDocument extends Document {
             strategy="beforeInteractive"
             src="/js/isotope.pkgd.min.js"
           ></Script>
-          {GOOGLE_TAG_MANAGER_ID && (
-            <Script id="google-tag-manager" strategy="afterInteractive">
-              {`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
-              `}
-            </Script>
-          )}
         </body>
       </Html>
     );
